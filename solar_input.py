7ce71db76ea9d7202776c9046f6f74a1bd258f2f
Max-xon <91071188+Max-xon@users.noreply.github.com>
2021-11-08 17:43:28 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,11 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = planet()
+                parse_star_parameters(line, planet)
+                objects.append(planet)
+
             else:
                 print("Unknown space object")
 
@@ -43,8 +48,16 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    body.R = int(line.split()[1])
+    body.color = line.split()[2]
+    body.m = int(line.split()[3])
+    body.x = int(line.split()[4])
+    body.y = int(line.split()[4])
+    body.Vx = int(line.split()[3])
+    body.Vy = int(line.split()[3])
 
-    pass  # FIXME: not done yet
+      # FIXME: not done yet
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +74,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    body.R = int(line.split()[1])
+    body.color = line.split()[2]
+    body.m = int(line.split()[3])
+    body.x = int(line.split()[4])
+    body.y = int(line.split()[4])
+    body.Vx = int(line.split()[3])
+    body.Vy = int(line.split()[3])
+      # FIXME: not done yet...
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -77,8 +97,9 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print(out_file, obj.+"")
             # FIXME: should store real values
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
