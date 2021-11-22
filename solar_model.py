c088ff6060ffefb20412484d1aa3e8a9e7850677
Max-xon <91071188+Max-xon@users.noreply.github.com>
2021-11-22 16:16:50 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -18,9 +18,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        f=(gravitational_constant*body.m*obj.m)/r**2
-        body.Fx += f*((body.y-obj.y)/r)  # FIXME: нужно вывести формулу...
-        body.Fy += f*((body.x-obj.x)/r)
+        body.Fx += -gravitational_constant*obj.m*body.m*(body.x - obj.x)/r**3
+        body.Fy += -gravitational_constant*obj.m*body.m*(body.y - obj.y)/r**3
         print(body.Fx) # FIXME: нужно вывести формулу...
 
 
@@ -32,12 +31,14 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
+    old = body.x
     ax = body.Fx/body.m
-    ay = body.Fy/body.m
+    body.x += body.Vx*dt + ax*dt**2/2
     body.Vx += ax*dt
-    body.Vy +=ay*dt
-    body.x += body.Vx*dt
-    body.y += body.Vy*dt # FIXME: не понимаю как менять...
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt + ay*dt**2/2
+    body.Vy += ay*dt
+ # FIXME: не понимаю как менять...
     
     # FIXME: not done recalculation of y coordinate!
 
